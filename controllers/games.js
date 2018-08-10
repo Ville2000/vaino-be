3197ca3a1ec775dcf286de5ef4f64c132e66b39e
Ville Haapavaara <ville.haapavaara@cs.tamk.fi>
2018-08-10 14:09:38 +0300

    Add method to retrieve game by id

--- controllers/games.js
+++ controllers/games.js
@@ -3,8 +3,9 @@ const Game = require('../models/game')
 const User = require('../models/user')
 const getDecodedToken = require('../utils/token')
 
+/** Create a new game */
 gamesRouter.post('/', async (req, res) => {
-  const decodedToken = getDecodedToken(req)
+  const decodedToken = await getDecodedToken(req)
 
   if (!(decodedToken || decodedToken.id))
     res.send(401).json({ error: 'Authorization token missing or invalid.' })
@@ -24,6 +25,54 @@ gamesRouter.post('/', async (req, res) => {
   await user.save()
 
   res.status(200).json(savedGame)
+})
+
+/** Add a player to a game */
+gamesRouter.put('/:id/addPlayer', async (req, res) => {
+  const gameId = req.params.id
+  const playerUsername = req.body.username
+  console.log('playerUsername', playerUsername)
+  
+  const decodedToken = await getDecodedToken(req)
+
+  if (!(decodedToken || decodedToken.id))
+    res.send(401).json({ error: 'Authorization token missing or invalid.' })
+
+  const game = await Game.findById(gameId)
+
+  if (!game)
+    res.send(404).json({ error: 'Game not found' })
+
+  const isCreator = game.createdBy == decodedToken.id
+
+  if (!isCreator)
+    res.send(401).json({ error: 'Not authorized' })
+
+  // TODO: Lisää pelaaja pelaajalistaan
+  // game.players = game.players.concat(player)
+  
+  res.send(200)
+})
+
+/** Get game by id */
+gamesRouter.get('/:id', async (req, res) => {
+  const decodedToken = await getDecodedToken(req)
+
+  if (!(decodedToken || decodedToken.id))
+    res.send(401).json({ error: 'Authorization token missing or invalid.' })
+
+  const gameId = req.params.id
+  const game = await Game.findById(gameId)
+
+  if (!game)
+    res.send(404).json({ error: 'Game not found' })
+
+  const includesPlayer = game.players.some(player => player == decodedToken.id)
+
+  if (!includesPlayer)
+    res.send(401).json({ error: 'Not authorized.' })
+
+  res.status(200).json(game)
 })
 
 module.exports = gamesRouter
